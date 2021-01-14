abd794a8d28733bb7b165300ba98fd0b8c44dd81
Ash <sgt16@yandex.ru>
2021-01-14 18:40:17 +0300

    1.6. Взаимодействие объектов [#431276]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void goAway(String say) {
+        System.out.println(say);
+    }
+
+    public void die() {
+        System.out.println("Ball DIED");
+    }
+}
