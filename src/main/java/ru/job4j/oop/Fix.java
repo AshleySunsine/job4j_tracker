791829ed647399f8443936ac01a3edd6e55573f3
Ash <sgt16@yandex.ru>
2021-01-14 18:49:53 +0300

    1.7. Конструктор [#431279]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String newDesc) {
+        desc = newDesc;
+    }
+
+    public static void main(String[] args) {
+        Fix fix = new Fix("Alalalala");
+    }
+}
