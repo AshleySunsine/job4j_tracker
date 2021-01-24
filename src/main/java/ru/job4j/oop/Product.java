122bdf1854a58a9a290f61a6aca4ddc53f7e101b
Ash <sgt16@yandex.ru>
2021-01-24 17:57:17 +0300

    4. Сравнение моделей. Метод equals [#431294]

--- src/main/java/ru/job4j/oop/Product.java
+++ src/main/java/ru/job4j/oop/Product.java
@@ -1,5 +1,7 @@
 package ru.job4j.oop;
 
+import java.util.Objects;
+
 public final class Product {
     private String name;
     private int price;
@@ -19,5 +21,18 @@ public final class Product {
 
     public String label() {
         return name + " " + price();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
