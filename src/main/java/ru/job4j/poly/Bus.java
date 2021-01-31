ff55da3ec9ac1aaec034e2e6d8518294c36f1d85
Ash <sgt16@yandex.ru>
2021-01-31 18:57:37 +0300

    3. Интерфейс Input [#431223]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void people(int count) {
+
+    }
+
+    @Override
+    public double people(double fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    public void drive();
+
+    public void people(int count);
+
+    public double people(double fuel);
+}
