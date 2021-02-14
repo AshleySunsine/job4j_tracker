78eeb8a0c90f94433076b090c3191af5b6fa3ba2
Ash <sgt16@yandex.ru>
2021-02-14 17:54:34 +0300

    2.1. Тест на ValidateInput [#431247]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -21,6 +21,9 @@ public class ValidateInput implements Input {
         do {
             try {
                 value = in.askInt(question);
+                if (value < 0) {
+                    throw new NumberFormatException("Введено отрицательное число!");
+                }
                 invalid = false;
             } catch (NumberFormatException nfe) {
                 out.println("Please enter validate data again.");
