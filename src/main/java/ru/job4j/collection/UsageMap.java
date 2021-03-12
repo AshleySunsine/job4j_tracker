430c41519271e833b9ff759c5b48a3fe9aecb358
Ash <sgt16@yandex.ru>
2021-03-12 23:08:19 +0300

    1. Паспорт и жители [#431209]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,10 +1,11 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<String, String>();
+        Map<String, String> map = new HashMap<String, String>();
         map.put("Drakula@bla.blabla", "Graph");
         map.put("Da@.net", "Da");
         for (String key : map.keySet()) {
