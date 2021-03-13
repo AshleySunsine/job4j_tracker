907e819c5c7d39ef61cbb31bbfc4cc9801f4c537
Ash <sgt16@yandex.ru>
2021-03-13 10:35:25 +0300

    1. Паспорт и жители [#431209]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
-
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -13,5 +15,15 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addDublicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenDubl = new Citizen("2f44a", "Olga Buzova");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizenDubl);
+        assertThat(office.get("2f44a").getUsername(), is("Petr Arsentev"));
     }
 }
